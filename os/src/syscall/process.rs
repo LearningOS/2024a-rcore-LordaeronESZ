0181646ced9912df0ff28864f423c2f006d0d9da
LordaeronESZ <1667510710@qq.com>
2024-10-16 09:51:47 +0800

    reimplement sys_get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,9 +1,11 @@
 //! Process management syscalls
+
+use core::{mem::size_of};
+
 use crate::{
-    config::MAX_SYSCALL_NUM,
-    task::{
-        change_program_brk, exit_current_and_run_next, suspend_current_and_run_next, TaskStatus,
-    },
+    config::MAX_SYSCALL_NUM, mm::translated_byte_buffer, task::{
+        change_program_brk, current_user_token, exit_current_and_run_next, suspend_current_and_run_next, TaskStatus
+    }, timer::get_time_us
 };
 
 #[repr(C)]
@@ -43,7 +45,21 @@ pub fn sys_yield() -> isize {
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
-    -1
+    let _us = get_time_us();
+    let tv = TimeVal {
+        sec: _us / 1_000_000,
+        usec: _us % 1_000_000,
+    };
+    let buffers = translated_byte_buffer(
+        current_user_token(), _ts as *const u8, size_of::<TimeVal>());
+    let mut tv_ptr = &tv as *const _ as *const u8;
+    for buffer in buffers {
+        unsafe {
+            tv_ptr.copy_to(buffer.as_mut_ptr(), buffer.len());
+            tv_ptr = tv_ptr.add(buffer.len());
+        }
+    }
+    0
 }
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
@@ -51,7 +67,8 @@ pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
 /// HINT: What if [`TaskInfo`] is splitted by two pages ?
 pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
     trace!("kernel: sys_task_info NOT IMPLEMENTED YET!");
-    -1
+    
+    0
 }
 
 // YOUR JOB: Implement mmap.
