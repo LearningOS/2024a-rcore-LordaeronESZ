2a624553fb4ae63ed8bfec72d2b69ee84873eaef
LordaeronESZ <1667510710@qq.com>
2024-10-16 09:51:47 +0800

    reimplement sys_get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,13 +1,16 @@
 //! Process management syscalls
+
+use core::mem::size_of;
+
 use alloc::sync::Arc;
 
 use crate::{
     config::MAX_SYSCALL_NUM,
     fs::{open_file, OpenFlags},
-    mm::{translated_refmut, translated_str},
+    mm::{translated_byte_buffer, translated_refmut, translated_str},
     task::{
         add_task, current_task, current_user_token, exit_current_and_run_next, suspend_current_and_run_next, TaskStatus
-    },
+    }, timer::get_time_us,
 };
 
 #[repr(C)]
@@ -116,11 +119,22 @@ pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
-        current_task().unwrap().pid.0
-    );
-    -1
+    trace!("kernel: sys_get_time");
+    let _us = get_time_us();
+    let tv = TimeVal {
+        sec: _us / 1_000_000,
+        usec: _us % 1_000_000,
+    };
+    let buffers = translated_byte_buffer(
+        current_user_token(), _ts as *const u8, size_of::<TimeVal>());
+    let mut tv_ptr = &tv as *const _ as *const u8;
+    for buffer in buffers {
+        unsafe {
+            tv_ptr.copy_to(buffer.as_mut_ptr(), buffer.len());
+            tv_ptr = tv_ptr.add(buffer.len());
+        }
+    }
+    0
 }
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
