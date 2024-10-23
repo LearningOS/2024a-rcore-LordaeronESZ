6f061585e24a680430eab4ee4764c2609a31f02a
LordaeronESZ <1667510710@qq.com>
2024-10-23 10:50:58 +0800

    [fix] add fd_table copy for spawn

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -352,6 +352,15 @@ impl TaskControlBlock {
         let pid_handle = pid_alloc();
         let kernel_stack = kstack_alloc();
         let kernel_stack_top = kernel_stack.get_top();
+        // copy fd table
+        let mut new_fd_table: Vec<Option<Arc<dyn File + Send + Sync>>> = Vec::new();
+        for fd in parent_inner.fd_table.iter() {
+            if let Some(file) = fd {
+                new_fd_table.push(Some(file.clone()));
+            } else {
+                new_fd_table.push(None);
+            }
+        }
         let task_control_block = Arc::new(TaskControlBlock {
             pid: pid_handle,
             kernel_stack,
@@ -367,7 +376,7 @@ impl TaskControlBlock {
                     exit_code: 0,
                     heap_bottom: parent_inner.heap_bottom,
                     program_brk: parent_inner.program_brk,
-                    fd_table: Vec::new(),
+                    fd_table: new_fd_table,
                     prio: 16,
                     stride: 0,
                     syscall_times: [0u32; MAX_SYSCALL_NUM],
